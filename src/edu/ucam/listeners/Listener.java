26d00ff0bb51d19a5ea7865e1e7581d9c5835481
Iván <48927047+saurasmaker@users.noreply.github.com>
2021-05-02 02:25:24 +0200

    finish

--- src/edu/ucam/listeners/Listener.java
+++ src/edu/ucam/listeners/Listener.java
@@ -1,23 +1,11 @@
 package edu.ucam.listeners;
 
-import javax.servlet.AsyncEvent;
-import javax.servlet.AsyncListener;
-import javax.servlet.ServletContextAttributeEvent;
-import javax.servlet.ServletContextAttributeListener;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import javax.servlet.ServletRequestAttributeEvent;
-import javax.servlet.ServletRequestAttributeListener;
+
 import javax.servlet.ServletRequestEvent;
 import javax.servlet.ServletRequestListener;
 import javax.servlet.annotation.WebListener;
-import javax.servlet.http.HttpSessionActivationListener;
-import javax.servlet.http.HttpSessionAttributeListener;
-import javax.servlet.http.HttpSessionBindingEvent;
-import javax.servlet.http.HttpSessionBindingListener;
-import javax.servlet.http.HttpSessionEvent;
-import javax.servlet.http.HttpSessionIdListener;
-import javax.servlet.http.HttpSessionListener;
 
 import edu.ucam.database.DatabaseController;
 import edu.ucam.enums.ErrorType;
@@ -27,7 +15,7 @@ import edu.ucam.enums.ErrorType;
  *
  */
 @WebListener
-public class Listener implements ServletContextAttributeListener, ServletContextListener, HttpSessionListener, HttpSessionAttributeListener, HttpSessionActivationListener, HttpSessionBindingListener, HttpSessionIdListener, ServletRequestListener, ServletRequestAttributeListener, AsyncListener {
+public class Listener implements ServletRequestListener {
 
     /**
      * Default constructor. 
@@ -36,61 +24,6 @@ public class Listener implements ServletContextAttributeListener, ServletContext
         // TODO Auto-generated constructor stub
     }
 
-	/**
-     * @see HttpSessionListener#sessionCreated(HttpSessionEvent)
-     */
-    public void sessionCreated(HttpSessionEvent arg0)  { 
-         // TODO Auto-generated method stub
-    }
-
-	/**
-     * @see ServletContextAttributeListener#attributeRemoved(ServletContextAttributeEvent)
-     */
-    public void attributeRemoved(ServletContextAttributeEvent arg0)  { 
-         // TODO Auto-generated method stub
-    }
-
-	/**
-     * @see AsyncListener#onError(AsyncEvent)
-     */
-    public void onError(AsyncEvent arg0) throws java.io.IOException { 
-         // TODO Auto-generated method stub
-    }
-
-	/**
-     * @see HttpSessionIdListener#sessionIdChanged(HttpSessionEvent, String)
-     */
-    public void sessionIdChanged(HttpSessionEvent arg0, String arg1)  { 
-         // TODO Auto-generated method stub
-    }
-
-	/**
-     * @see ServletRequestAttributeListener#attributeAdded(ServletRequestAttributeEvent)
-     */
-    public void attributeAdded(ServletRequestAttributeEvent arg0)  { 
-         // TODO Auto-generated method stub
-    }
-
-	/**
-     * @see AsyncListener#onTimeout(AsyncEvent)
-     */
-    public void onTimeout(AsyncEvent arg0) throws java.io.IOException { 
-         // TODO Auto-generated method stub
-    }
-
-	/**
-     * @see HttpSessionAttributeListener#attributeReplaced(HttpSessionBindingEvent)
-     */
-    public void attributeReplaced(HttpSessionBindingEvent arg0)  { 
-         // TODO Auto-generated method stub
-    }
-
-	/**
-     * @see HttpSessionActivationListener#sessionWillPassivate(HttpSessionEvent)
-     */
-    public void sessionWillPassivate(HttpSessionEvent arg0)  { 
-         // TODO Auto-generated method stub
-    }
 
 	/**
      * @see ServletContextListener#contextInitialized(ServletContextEvent)
@@ -100,47 +33,7 @@ public class Listener implements ServletContextAttributeListener, ServletContext
 		if(et != ErrorType.NO_ERROR) System.out.println(et);
     }
 
-	/**
-     * @see ServletContextAttributeListener#attributeAdded(ServletContextAttributeEvent)
-     */
-    public void attributeAdded(ServletContextAttributeEvent event)  { 
-         // TODO Auto-generated method stub
-    }
-
-	/**
-     * @see AsyncListener#onComplete(AsyncEvent)
-     */
-    public void onComplete(AsyncEvent event) throws java.io.IOException { 
-         // TODO Auto-generated method stub
-    }
-
-	/**
-     * @see ServletRequestListener#requestDestroyed(ServletRequestEvent)
-     */
-    public void requestDestroyed(ServletRequestEvent arg0)  { 
-         // TODO Auto-generated method stub
-    }
-
-	/**
-     * @see ServletRequestAttributeListener#attributeRemoved(ServletRequestAttributeEvent)
-     */
-    public void attributeRemoved(ServletRequestAttributeEvent srae)  { 
-         // TODO Auto-generated method stub
-    }
-
-	/**
-     * @see AsyncListener#onStartAsync(AsyncEvent)
-     */
-    public void onStartAsync(AsyncEvent event) throws java.io.IOException { 
-         // TODO Auto-generated method stub
-    }
-
-	/**
-     * @see HttpSessionBindingListener#valueBound(HttpSessionBindingEvent)
-     */
-    public void valueBound(HttpSessionBindingEvent arg0)  { 
-         // TODO Auto-generated method stub
-    }
+	
 
 	/**
      * @see ServletRequestListener#requestInitialized(ServletRequestEvent)
@@ -153,60 +46,12 @@ public class Listener implements ServletContextAttributeListener, ServletContext
     	}
     }
 
-	/**
-     * @see HttpSessionListener#sessionDestroyed(HttpSessionEvent)
-     */
-    public void sessionDestroyed(HttpSessionEvent se)  { 
-         // TODO Auto-generated method stub
-    }
 
-	/**
-     * @see HttpSessionActivationListener#sessionDidActivate(HttpSessionEvent)
-     */
-    public void sessionDidActivate(HttpSessionEvent se)  { 
-         // TODO Auto-generated method stub
-    }
+	@Override
+	public void requestDestroyed(ServletRequestEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 
-	/**
-     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
-     */
-    public void contextDestroyed(ServletContextEvent sce)  { 
-         // TODO Auto-generated method stub
-    }
-
-	/**
-     * @see ServletRequestAttributeListener#attributeReplaced(ServletRequestAttributeEvent)
-     */
-    public void attributeReplaced(ServletRequestAttributeEvent srae)  { 
-         // TODO Auto-generated method stub
-    }
-
-	/**
-     * @see HttpSessionAttributeListener#attributeAdded(HttpSessionBindingEvent)
-     */
-    public void attributeAdded(HttpSessionBindingEvent event)  { 
-         // TODO Auto-generated method stub
-    }
-
-	/**
-     * @see HttpSessionAttributeListener#attributeRemoved(HttpSessionBindingEvent)
-     */
-    public void attributeRemoved(HttpSessionBindingEvent event)  { 
-         // TODO Auto-generated method stub
-    }
-
-	/**
-     * @see ServletContextAttributeListener#attributeReplaced(ServletContextAttributeEvent)
-     */
-    public void attributeReplaced(ServletContextAttributeEvent event)  { 
-         // TODO Auto-generated method stub
-    }
-
-	/**
-     * @see HttpSessionBindingListener#valueUnbound(HttpSessionBindingEvent)
-     */
-    public void valueUnbound(HttpSessionBindingEvent arg0)  { 
-         // TODO Auto-generated method stub
-    }
 	
 }
